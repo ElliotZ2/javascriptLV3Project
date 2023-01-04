6e545f4377d11c6dac4c5fe5e6590ca3ef581797
ElliotZ2 <98603591+ElliotZ2@users.noreply.github.com>
2023-01-03 22:11:34 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ class Fighter {
     cunning = 6;
     speed = 6;
     fatigue = 30;
+    maxFatigue;
     name;
+    isAlive = true;
 
     constructor(name) {
         this.name = name;
@@ -31,6 +33,7 @@ class Fighter {
         this.cunning = arrValues[1];
         this.speed = arrValues[2];
         this.fatigue = arrValues[3];
+        this.maxFatigue = this.fatigue;
     }
 
     logStats() {
@@ -38,7 +41,11 @@ class Fighter {
         console.log(this.strength);
         console.log(this.cunning);
         console.log(this.speed);
-        console.log(this.fatigue);
+        console.log("Fatigue: " + this.fatigue);
+    }
+
+    stats() {
+        return "Strength:" + this.strength + " Cunning:" + this.cunning + " Speed:" + this.speed;
     }
 
     attack() {
@@ -51,26 +58,223 @@ class Fighter {
         if (isDefending) {
             defenseLevel = this.speed + this.cunning;
         }
-        else{
+        else {
             defenseLevel = this.speed + Math.floor(Math.random() * 6) + 1;
         }
         return defenseLevel;
     }
-}
 
-var fighter1 = new Fighter("joe");
-var fighter2 = new Fighter("jo");
-fighter1.logStats();
-fighter2.logStats();
+    takeDamage(damage) {
+        this.fatigue -= damage;
+        if (this.fatigue < 0) {
+            this.fatigue = 0;
+        }
+    }
 
-function initialize() {
-    var playerFighter = new Fighter("player");
-    var computerFighter = new Fighter("computer");
-    while (fighter1.fatigue > 0 && fighter2.fatigue > 0) {
-
+    heal(value) {
+        this.fatigue += value;
+        if (this.fatigue > this.maxFatigue) {
+            this.fatigue = this.maxFatigue;
+        }
     }
 }
 
-function playerAttack() {
-    
+var playerFighter;
+var computerFighter;
+var computerBirdName;
+var playerBirdName;
+var playerFighterAttackVal;
+var computerFighterAttackVal;
+var playerFighterDefenseVal;
+var computerFighterDefenseVal;
+var playerAttemptFinisher = false;
+var computerAttemptFinisher = false;
+var gameOver = false;
+const birds = ["baldeagle", "chicken", "crow", "flamingo", "goose", "hummingbird", "owl", "parrot", "penguin", "toucan", "turkey"];
+
+function initialize() {
+    playerBirdName = birds[Math.floor(Math.random() * birds.length)];
+    computerBirdName = birds[Math.floor(Math.random() * birds.length)];
+    while (playerBirdName == computerBirdName) {
+        computerBirdName = birds[Math.floor(Math.random() * birds.length)];
+    }
+    playerFighter = new Fighter(playerBirdName);
+    computerFighter = new Fighter(computerBirdName);
+    document.getElementById("fighter1").src = "images/" + playerBirdName + ".jpg";
+    document.getElementById("fighter1nametag").innerHTML = "Player: " + playerBirdName;
+    document.getElementById("fighter2").src = "images/" + computerBirdName + ".jpg";
+    document.getElementById("fighter2nametag").innerHTML = "Computer: " + computerBirdName;
+    logToBattleText("The battle between the " + playerBirdName + " and the " + computerBirdName + " begins!");
+    logToBattleText("Stats of player's " + playerBirdName + " stats: " + playerFighter.stats());
+    logToBattleText("Stats of computer's " + computerBirdName + " stats: " + computerFighter.stats());
+    document.getElementById("finisherbutton").disabled=true;
+}
+
+function logToBattleText(text) {
+    document.getElementById("battletext").innerHTML += text + "<br />";
+}
+
+function playerAttack() {//when the user clicks the attack button
+    if(gameOver) {return;}
+    playerFighterAttackVal = playerFighter.attack();
+    playerFighterDefenseVal = playerFighter.speed + playerFighter.cunning;
+    computerTurn();
+    playRound();
+}
+
+function playerDefend() {//user clicks the defend button
+    if(gameOver) {return;}
+    playerFighterAttackVal = 0;
+    playerFighterDefenseVal = playerFighter.speed + Math.floor(Math.random() * 6) + 1;
+    computerTurn();
+    playRound();
+}
+
+function playerFinisher() {//user clicks the finisher button
+    playerAttemptFinisher = true;
+    computerTurn();
+    playRound();
+}
+
+function computerTurn() {
+    if (playerFighter.fatigue <= 0 || playerFighter.fatigue * 2 <= computerFighter.fatigue) {
+        computerAttemptFinisher = true;
+    }
+    else {
+        choice = Math.floor(Math.random() * 2);
+        if (choice) {
+            //attack
+            console.log("computer attacks");
+            computerFighterAttackVal = computerFighter.attack();
+            computerFighterDefenseVal = computerFighter.speed + computerFighter.cunning;
+        }
+        else {
+            //defend
+            console.log("computer defends");
+            computerFighterAttackVal = 0;
+            computerFighterDefenseVal = computerFighter.speed + Math.floor(Math.random() * 6) + 1;
+        }
+    }
+}
+
+function playRound() {//gets called to compute values everytime the player clicks the attack, defend, or finisher option
+    if(playerAttemptFinisher && computerAttemptFinisher) {
+        let random = Math.floor(Math.random() * 3) + 1;
+        let finisherAttackVal = (playerFighter.strength + playerFighter.speed) / random;
+        if(finisherAttackVal > computerFighterDefenseVal) {
+            gameOver = true;
+            logToBattleText("The player's " + playerBirdName + " has finished the " + computerBirdName + ".");
+            logToBattleText("YOU WIN");
+            document.getElementById("header").innerHTML="YOU WIN!";
+            return;
+        }
+        else{
+            random = Math.floor(Math.random() * 3) + 1;
+            finisherAttackVal = (computerFighter.strength + computerFighter.speed) / random;
+            if(finisherAttackVal > playerFighterDefenseVal) {
+                gameOver = true;
+                logToBattleText("The computer's " + computerBirdName + " has finished the " + playerBirdName + ".");
+                logToBattleText("YOU LOSE");
+                document.getElementById("header").innerHTML="YOU LOSE!";
+                return;
+            }
+            else{
+                logToBattleText("Both the player's " + playerBirdName + " and computer's + " + computerBirdName + " finishers failed!");
+            }
+        }
+
+    }
+    else if(playerAttemptFinisher) {
+        let random = Math.floor(Math.random() * 3) + 1;
+        let finisherAttackVal = (playerFighter.strength + playerFighter.speed) / random;
+        if(finisherAttackVal > computerFighterDefenseVal) {
+            gameOver = true;
+            logToBattleText("The player's " + playerBirdName + " has finished the " + computerBirdName + ".");
+            logToBattleText("YOU WIN");
+            document.getElementById("header").innerHTML="YOU WIN!";
+        }
+        else{
+            let healValue = Math.floor(Math.random() * 6) + 1;
+            logToBattleText("The computer's " + computerBirdName + " narrowly escaped being finished and healed for " + healValue + " health.");
+            computerFighter.heal(healValue);
+        }
+    }
+    else if(computerAttemptFinisher) {
+        let random = Math.floor(Math.random() * 3) + 1;
+        let finisherAttackVal = (computerFighter.strength + computerFighter.speed) / random;
+        if(finisherAttackVal > playerFighterDefenseVal) {
+            gameOver = true;
+            logToBattleText("The computer's " + computerBirdName + " has finished the " + playerBirdName + ".");
+            logToBattleText("YOU LOSE");
+            document.getElementById("header").innerHTML="YOU LOSE!";
+        }
+        else{
+            let healValue = Math.floor(Math.random() * 6) + 1;
+            logToBattleText("The player's " + playerBirdName + " narrowly escaped being finished and healed for " + healValue + "health.");
+            playerFighter.heal(healValue);
+        }
+    }
+    playerAttemptFinisher = false;
+    computerAttemptFinisher = false;
+    if(playerFighterAttackVal === 0 && computerFighterAttackVal === 0) {//both defend
+        logToBattleText("Both birds defended.");
+        let healValue = Math.floor(Math.random() * 6) + 1;
+        playerFighter.heal(healValue);
+        logToBattleText("The player's " + playerBirdName + " healed for " + healValue + " health.");
+        healValue = Math.floor(Math.random() * 6) + 1;
+        computerFighter.heal(healValue);
+        logToBattleText("The computer's " + computerBirdName + " healed for " + healValue + " health.");
+    }
+    else if(playerFighterAttackVal === 0 && computerFighterAttackVal > 0) {//only computer attacks, player defends
+        let damageTakenByPlayer = playerFighterDefenseVal - computerFighterAttackVal;
+        if(damageTakenByPlayer <= 0) {//successful defense
+            let healValue = Math.floor(Math.random() * 6) + 1;
+            playerFighter.heal(healValue);
+            logToBattleText("The player's " + playerBirdName + " defended the " + computerBirdName + "\'s attack and healed for " + healValue + " health.");
+        }
+        else{
+            playerFighter.takeDamage(damageTakenByPlayer);
+            logToBattleText("The player's " + playerBirdName + " defended but still took " + damageTakenByPlayer + " damage from the " + computerBirdName + ".");
+        }
+    }
+    else if(playerFighterAttackVal > 0 && computerFighterAttackVal === 0) {//only player attacks, computer defends
+        let damageTakenByComputer = computerFighterDefenseVal - playerFighterAttackVal;
+        if(damageTakenByComputer <= 0) {
+            let healValue = Math.floor(Math.random() * 6) + 1;
+            computerFighter.heal(healValue);
+            logToBattleText("The computer's " + computerBirdName + " defended the " + playerBirdName + "\'s attack and healed for " + healValue + " health.");
+        }
+        else{
+            computerFighter.takeDamage(damageTakenByComputer);
+            logToBattleText("The computer's " + computerBirdName + " defended but still took " + damageTakenByComputer + " damage from the " + playerBirdName + ".");
+        }
+    }
+    else{//both computer and player attacks
+        let playerAttackDamage = Math.max(0, computerFighterDefenseVal - playerFighterAttackVal);
+        logToBattleText("The player's " + playerBirdName + " attacked the " + computerBirdName + " for " + playerAttackDamage + ".");
+        computerFighter.takeDamage(playerAttackDamage);
+        let computerAttackDamage = Math.max(0, playerFighterDefenseVal - computerFighterAttackVal);
+        logToBattleText("The computer's " + computerBirdName + " attacked the " + playerBirdName + " for " + computerAttackDamage + ".");
+        playerFighter.takeDamage(computerAttackDamage);
+    }
+
+    document.getElementById("fighter1health").style.width = "" + (Math.max(0, playerFighter.fatigue / playerFighter.maxFatigue) * 100) + "%";
+    document.getElementById("fighter2health").style.width = "" + (Math.max(0, computerFighter.fatigue / computerFighter.maxFatigue) * 100) + "%";
+    console.log("player" + playerFighter.fatigue);
+    console.log("computer" + computerFighter.fatigue);
+    document.getElementById("fighter1fatiguetext").innerHTML = "Fatigue: " + playerFighter.fatigue;
+    document.getElementById("fighter2fatiguetext").innerHMTL = "Fatigue: " + computerFighter.fatigue;
+    document.getElementById("fighter2fatiguetext").innerHMTL = "WHAT IS THIS?";
+
+    checkForPlayerFinisher();
+}
+
+function checkForPlayerFinisher() {//TODO put it also in the button that calls the finisher
+    if((playerFighter.fatigue >= computerFighter.fatigue * 2) || (computerFighter.fatigue === 0)) {//player may attempt a finishing move, button appears
+        document.getElementById("finisherbutton").disabled=false;
+    }
+    else{
+        console.log("worked");
+        document.getElementById("finisherbutton").disabled=true;
+    }
 }
